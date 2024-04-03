808d9659b9ca7ba883ca15f61ee418117df4835c
Basil kelly <basiljkelly@outlook.com>
2024-04-03 10:53:15 +0100

    changed deals to update when it is focused to fix votes values not updating

--- Components/SingleDeal.jsx
+++ Components/SingleDeal.jsx
@@ -115,7 +115,6 @@ const SingleDeal = ({ route }) => {
       } else if (voteType === "down") {
         voteIncrement = -1;
       }
-      console.log(dealData.votes)
       const { data, error } = await supabase
         .from("deals")
         .update({ votes: dealData.votes + voteIncrement })
